16893a76b5abc48a9e96470694adff581b121bed
harish deepu <harish deepu@DESKTOP-IAQNSHU>
2022-06-10 03:23:33 -0700

    first commit

---
+++ JAVA PROGRAMMING/src/JAVABASICS/DataTypes.java
@@ -0,0 +1,20 @@
+package JAVABASICS;
+
+public class DataTypes {
+
+	public static void main(String[] args) {
+		// Data types in java
+		//Integer float double character
+		int a = 1;
+		float b = 1;
+		String c = "hello deepu";
+		double d = 98;
+		System.out.println(a);
+		System.out.println(b);
+		System.out.println(c);
+		System.out.println(d);
+		
+
+	}
+
+}
