e0c2d00914504a49febc643599f95fb22834f903
harish deepu <harish deepu@DESKTOP-IAQNSHU>
2022-06-10 03:13:48 -0700

    first commit

---
+++ JAVA PROGRAMMING/src/JAVABASICS/HelloWorld.java
@@ -0,0 +1,11 @@
+package JAVABASICS;
+
+public class HelloWorld {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hello World Deepu");
+       
+	}
+
+}
